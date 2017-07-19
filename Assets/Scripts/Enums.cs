1f0a4f341afb87cfe2964b6c097a292bb77e463d
Will Da Silva <will@willdasilva.xyz>
2017-07-18 21:51:24 -0400

    Change Enums.cs and others
    Minor formatting change in BattleManager.cs
    Added enum StatusEffectType to Enums
    Moved StatusEffect.cs and its meta file to new folder StatusEffects

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -1,54 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum StatusEffects
+{
+    Consumable,
+    Badge,
+    Important
+}
 
-public enum ItemTypes { Consumable, Badge, Important }
+public enum StatusEffectType
+{
+    Positive, // Buff
+    Neutral,
+    Negative // Debuff
+}
 
-//Status effects are not an enum
+public enum StatusEffectReapplyBehaviour
+{
+    No_Effect, // Effect cannot be applied when already afflicted,
+    Stack, // Stacks the effects of it (Ex: can be burned multiple times)
+    Extend, // The duration of the effect is increased
+    Burst, // Does an instantaneous effect
+    Reset, // Resets the timer until it is removed
+    Remove // Removes the effect
+}
 
 public enum StatusEffects
 {
-    Asleep, Drowsy, Blind, Burned,
-    DEF_Down, DEF_Up, Dizzy, Agile, Electrified,
-    Frozen, Large, Immobilized, Invisible,
-    ATK_Down, ATK_Up, Spiked, Charmed,
-    Poisoned, Regen, Shrunk, NoItems,
-    //Permanent Status Effects
-
-    //Health Effects
-    Danger, Angry
+    Asleep,
+    Drowsy,
+    Blind,
+    Burned,
+    DEF_Down,
+    DEF_Up,
+    Dizzy,
+    Agile,
+    Electrified,
+    Frozen,
+    Large,
+    Immobilized,
+    Invisible,
+    ATK_Down,
+    ATK_Up,
+    Spiked,
+    Charmed,
+    Poisoned,
+    Regen,
+    Shrunk,
+    No_Items,
+    Danger,
+    Angry
 }
 
-/*
-Alternate StatusEffects enum setup
-
-public enum AltStatusEffects
-{
-    SLP, BLD, BRN,
-    DEF_Down, DEF_Up, DZY, AGL, ELC,
-    Fast, FRZ, Big, IMM, INV,
-    ATK_Down, ATK_Up, Spiked,
-    PSN, Regen, Shrunk, Slow, Tiny,
-    //Permanent Status Effects
-    Spiky,
-    //Health Effects
-    Danger, Angry
-}
-*/
 public enum Elements
 {
-    None, Fire, Ice, Wind, Explosion
+    None,
+    Fire,
+    Ice,
+    Wind,
+    Explosion
 }
 
-public enum GroundStates { ground, underground, floating, flying, ceiling }
+public enum GroundStates
+{
+    Ground,
+    Underground,
+    Floating,
+    Flying,
+    Ceiling
+}
 
-public enum AttackablePos { front, any }
+public enum AttackablePos
+{
+    Front,
+    Back,
+    Any
+}
 
-public enum BattleStates { idle, player, enemy, dialogue } //From really early on. Not even sure if we'll need these anymore.
-
-public enum StatusEffectReapplyBehaviour { No_Effect,Stack,Reset,Burst,Remove} //No_Effect means that it cannot be applied when already afflicted, 
-                                                                               //Stack stacks the effects of it (Ex: can be burned multiple times)
-                                                                               //Reset resets the timer until it is removed
-                                                                               //Burst does an instantaneous effect
-                                                                               //Remove removes the effect
-
+public enum BattleStates //From really early on. Not even sure if we'll need these anymore.
+{
+    Idle,
+    Player,
+    Enemy,
+    Dialogue
+}
+>>>>>>> Stashed changes
