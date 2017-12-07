2a2c0ee528e827af563e2f523c2c5601c544bc35
NeatCrown <chol_peter@msn.com>
2017-12-07 12:33:49 -0800

    Modify 4WaySpriteRotation
    Change scaling to Billboarder's transform and changed LerpAngle to SmoothDampAngle

--- Assets/Scripts/Movement/_4WaySpriteRotation.cs
+++ Assets/Scripts/Movement/_4WaySpriteRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class _4WaySpriteRotation : MonoBehaviour
 {
+    float v;
     public float targetAngle;
     [SerializeField]
     public float currentAngle;// {get; private set;}
@@ -12,17 +13,20 @@ public class _4WaySpriteRotation : MonoBehaviour
     Billboarder billboarder;
     void Awake()
     {
-        billboarder = GetComponent<Billboarder>();
+        billboarder = GetComponentInChildren<Billboarder>();
     }
     //angles that the model/sprite will "face"
     //↖ ↗
     //↙ ↘
-    void FixedUpdate()
+    protected virtual void FixedUpdate()
     {
         //Mathf.LerpAngle()
         targetAngle = Mathf.Repeat(targetAngle, 360);
+        float targetA = targetAngle;
         //if ()
-        currentAngle = Mathf.LerpAngle(currentAngle, targetAngle, t);
+        //currentAngle = Mathf.LerpAngle(currentAngle, targetA, t);
+
+        currentAngle = Mathf.SmoothDampAngle(currentAngle, targetA, ref v, t);
         currentAngle = Mathf.Repeat(currentAngle, 360);
 
         transform.localEulerAngles = currentAngle * 2 * Vector3.up;
@@ -31,21 +35,21 @@ public class _4WaySpriteRotation : MonoBehaviour
 
         #region scaling
         float totaZScale = billboarder.zScale;// * billboarder.camSide;
-        if (currentAngle > 0 && currentAngle < 90)        //backright to backleft
+        if (currentAngle > 0 && currentAngle < 90)        //backleft to topleft
         {
-            transform.localScale = new Vector3(billboarder.camSide, 1, totaZScale);
+            billboarder.transform.localScale = new Vector3(totaZScale * billboarder.camSide, 1, totaZScale);
         }
-        else if (currentAngle > 90 && currentAngle < 180) //backleft to topleft
+        else if (currentAngle > 90 && currentAngle < 180) //topleft to topright
         {
-            transform.localScale = new Vector3(-totaZScale, 1, totaZScale);
+            billboarder.transform.localScale = new Vector3(-1, 1, totaZScale);
         }
-        else if (currentAngle > 180 && currentAngle < 270)//topleft to topright
+        else if (currentAngle > 180 && currentAngle < 270)//topright to backright
         {
-            transform.localScale = new Vector3(-billboarder.camSide, 1, totaZScale);
+            billboarder.transform.localScale = new Vector3(-totaZScale * billboarder.camSide, 1, totaZScale);
         }
-        else if (currentAngle > 270 && currentAngle < 720)//topright to backright
+        else if (currentAngle > 270 && currentAngle < 360)//backright to backleft
         {
-            transform.localScale = new Vector3(totaZScale, 1, totaZScale);
+            billboarder.transform.localScale = new Vector3(1, 1, totaZScale);
         }
         #endregion
     }
