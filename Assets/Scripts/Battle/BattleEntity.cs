7f6a4cd684c91abd294eb13944e34f452d264689
NeatCrown <chol_peter@msn.com>
2017-12-18 23:52:49 -0800

    Modify BattleEntity
    Changed temporatyImmunities and permanentImmunities from LinkedHashSet to HashSet, Added HasStatusEffect(), GiveStatusEffect(), RemoveStatusEffect(), IsImmuneTo(), IsWeakto(), TryDamage(), StartDamageAnimation() and TryIfnflictStatus(). Honestly, too much at once.

--- Assets/Scripts/Battle/BattleEntity.cs
+++ Assets/Scripts/Battle/BattleEntity.cs
@@ -13,21 +13,22 @@ public class BattleEntity
     public int ID;
     public int currentHealth, maxhealth;
     public int baseAttack, currentAttack;
-    public int defence;
+    public int baseDefence, currentDefence;
     public bool healthIsVisible;
+    public bool isPlayer;
 
     public Dictionary<StatusEffects, StatusEffect> statusEffects = new Dictionary<StatusEffects, StatusEffect>();
     public Dictionary<StatusEffects, StatusEffect> permanentEffects = new Dictionary<StatusEffects, StatusEffect>();
 
-    public LinkedHashSet<StatusEffects> permanentImmunities = new LinkedHashSet<StatusEffects>();
-    public LinkedHashSet<StatusEffects> temporaryImmunities = new LinkedHashSet<StatusEffects>();
+    public HashSet<StatusEffects> permanentImmunities = new HashSet<StatusEffects>();
+    public HashSet<StatusEffects> temporaryImmunities = new HashSet<StatusEffects>();
     public float jumpAttackOffset;
     public Animator animator;
     public Elements element;
     public GroundStates groundState;
     public List<Attack> attacks;
     public bool canAttack;
-
+    public bool allowDeath;
 
     /*public bool IsGrounded()
     {
@@ -36,7 +37,7 @@ public class BattleEntity
 
     public void GiveHealth(int amount)
     {
-
+        currentHealth = Mathf.Min(maxhealth + amount, maxhealth);
     }
     public bool HasStatusEffect(StatusEffects effect)
     {
@@ -46,18 +47,113 @@ public class BattleEntity
             return true;
         return false;
     }
-
-    public static void TryDamage(BattleEntity user, BattleEntity target, Attack attack, int power)
+    public bool HasStatusEffect(StatusEffect effect)
     {
-        int userDamage = user.currentAttack + power;
-        int totalDamage = Mathf.Max(userDamage-target.defence, 0);
-        if (target.element == attack.element)
+        return HasStatusEffect(effect.effect);
+    }
+
+    public void GiveStatusEffect(StatusEffect effect)
+    {
+        if (HasStatusEffect(effect))
+            statusEffects.Add(effect.effect, effect);
+        else
+            statusEffects[effect.effect] = effect;
+    }
+    public void GiveStatusEffects(StatusEffect[] effects)
+    {
+        foreach (StatusEffect eff in effects)
+            GiveStatusEffect(eff);
+    }
+
+    public void RemoveStatusEffect(StatusEffects effect)
+    {
+        statusEffects.Remove(effect);
+    }
+
+    public void RemoveStatusEffects(StatusEffects[] effects)
+    {
+        foreach (StatusEffects effect in effects)
+            RemoveStatusEffect(effect);
+    }
+    public bool IsImmuneTo(StatusEffects effect)
+    {
+        if (temporaryImmunities.Contains(effect) || permanentImmunities.Contains(effect))
+            return true;
+        return false;
+    }
+    public bool IsImmuneTo(StatusEffect effect)
+    {
+        return IsImmuneTo(effect.effect);
+    }
+    bool IsWeakTo(Elements element)
+    {
+        return false;//replace with elemt damage calculation
+    }
+    public void TryDamage(BattleEntity target, Attack attack, int basePower)
+    {
+        int totalDamage = basePower;
+        //if (IsWeakTo(attack.element))
+        //baseDamage += 2;
+
+        if (!attack.piercing && basePower > currentDefence)
+            totalDamage = Mathf.Max(basePower - currentDefence, 0);
+        else if (basePower <= currentDefence)
+            totalDamage = 0;
+        else totalDamage = basePower;
+
+        /*if (target.element == attack.element && attack.element != Elements.None)
+        {
             target.GiveHealth(totalDamage);
-        else target.currentHealth = Mathf.Max(target.currentHealth - totalDamage, 0);
+            target.animator.Play("PoweredUp");
+        }*/
+        if (totalDamage != 0)
+        {
+            currentHealth = Mathf.Max(currentHealth - totalDamage, 0);
+            target.animator.Play("Damaged");
+        }
+        else 
+        {
+            //play 'no damage' particle effects
+            target.animator.Play("Undamaged");
+        }
 
     }
-    public static void TryIfnflictStatus()
+    public void TryIfnflictStatus(StatusEffect effect)
+    {
+        bool hasEffect = HasStatusEffect(effect);
+        if (!hasEffect && !IsImmuneTo(effect))
+            statusEffects.Add(effect.effect, effect);
+        else if (hasEffect)
+            statusEffects[effect.effect] = effect;
+    }
+
+    void DoDamageAnimation()
     {
 
     }
+    IEnumerator StartDamageAnimation(ActionAnimation a)
+    {
+        animator.Play("Damaged");//“Damaged”
+        if (currentHealth != 0)
+        {
+            yield return new WaitForSeconds(2);
+            animator.Play("Idle");
+        }
+
+        /*else while (currentHealth == 0)
+        {
+            if (allowDeathAnimation)
+            {
+                animator.Play("Death");
+                break;
+            }
+                yield return new WaitForFixedUpdate();
+        }*/
+        yield return new WaitForSeconds(1);
+        if (currentHealth > 0)
+            animator.Play("Idle");
+        //else yield return new WaitUntil(a.isDone);
+        animator.Play("Death");
+    }
+
 }
