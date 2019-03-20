f3702f6c59973398832155636bac28a4ee25f7cb
NeatCrown <chol_peter@msn.com>
2019-03-19 17:49:32 -0700

    Modify Enums.cs
    Added summaries to EffectActivationTrigger and StatusEffectReapplyBehaviour.
    Changed and added descriptive comments to the StatusEffect enum values.
    Added "Electric" to Elements.

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -5,13 +5,31 @@ public enum ItemType
     Badge,
     Important
 }
+public enum HealthItemType
+{
+    Food,
+    Drink,
+    Other
+}
 
 public enum EffectActivationTrigger
 {
+    /// <summary>
+    /// Before any entities have gone. Technically also counts as after all entities have gotten a turn.
+    /// </summary>
     Turn_Start,
+    /// <summary>
+    /// When entity begins its own turn
+    /// </summary>
     Move_Start,
-    On_Hit, // Entity gets hit by enemy
-    On_Attack // Entity attacks enemy
+    /// <summary>
+    /// When entity gets attacked by enemy
+    /// </summary>
+    On_Hit,
+    /// <summary>
+    /// When entity attacks enemy
+    /// </summary>
+    On_Attack
 }
 
 public enum StatusEffectType
@@ -23,41 +41,72 @@ public enum StatusEffectType
 
 public enum StatusEffectReapplyBehaviour
 {
-    No_Effect, // Effect cannot be applied when already afflicted,
-    Stack, // Stacks the effects of it (Ex: can be burned multiple times)
-    Extend, // The duration of the effect is increased
-    Burst, // Does an instantaneous effect
-    Reset, // Resets the timer until it is removed
-    Remove // Removes the effect
+    /// <summary>
+    /// Effect cannot be applied when already afflicted
+    /// </summary>
+    No_Effect,
+    /// <summary>
+    /// Stacks the effects of it (Ex: can be burned multiple times)
+    /// </summary>
+    Stack,
+    /// <summary>
+    /// The duration of the effect is increased
+    /// </summary>
+    Extend,
+    /// <summary>
+    /// Does an instantaneous effect
+    /// </summary>
+    Burst,
+    /// <summary>
+    /// Resets the timer until it is removed
+    /// </summary>
+    Reset,
+    /// <summary>
+    /// Removes the effect
+    /// </summary>
+    Remove
 }
 
 public enum StatusEffects
 {
-    Asleep,     //Immoblile. Being hit has chance of waking affected up
-    Drowsy,     //Will fall asleep in 1 turn
     Blind,      //Acton command prompts are harder to see
-    Burned,     //Afflicted will take fire damage each turn
-    DEF_Down,   //Decreases defence of the afflicted
-    DEF_Up,     //Increases defence of the afflicted
+
+    Burned,     //Afflicted will take fire damage each turn. Either water or ice will put it out.
+    Poisoned,   //Afflicted takes damage every turn; +1 per level.
+
+    ATK_Down,   //Decreases attack of the afflicted by -1 per level
+    ATK_Up,     //Increases attack of the afflicted by +1 per level
+    DEF_Down,   //Decreases defence of the afflicted by -1 per level
+    DEF_Up,     //Increases defence of the afflicted by +1 per level
+
     Dizzy,      //Afflicted will take random attacks
     Agile,      //Afflicted has a chance of dodging attacks
     Fast,       //Can do 2 actions in 1 turn
     Slow,       //Do 1 action every second turn
-    Electrified,
-    Frozen,     //Renders affliced immobile. Fire will cancel this status
-    Large,      //+2 attack power
-    Shrunk,     //-2 Attack power
+
+    Drowsy,     //Will fall asleep in 1 turn.
+    Asleep,     //Immoblile. Either being hit or a full round ending has chance of waking affected up. Level determines the chance of being woken up (chance = 1 / 2^level) At most, 4 turns may pass.
     Immobilized,//Unable to do any actions
-    Invisible,  //Intangible, can't be hit
-    ATK_Down,   //Decreases attack
-    ATK_Up,     //Increases attack
-    Spiked,     //Can't be hit by physical attacks
-    Charmed,    //
-    Poisoned,   //Afflicted takes damage every turn
-    Regen,      //Regenerates health
+    Frozen,     //Renders affliced immobile. Fire attacks will cancel this status
+    Petrified,  //Renders affliced immobile, but defence is increased by 1 point per status level
+
+    Trapped,    //Makes it so the afflicted can't run away
+
+    Grown,      //+1 attack power per status effect level
+    Shrunk,     //-1 Attack power per status effect level
+
+    Invisible,  //entity can't be seen, can still be attacked if timed correctly
+    Intangible, //Can't be hit
+
+    Spiked,     //Can't be hit by physical attacks and does normal damage on the attacker
+    Electrifying,//Can't be hit by physical attacks and inflicts electrical damage on the attacker.
+
+    Charmed,    //Affected will normally refuse to attack the caster, but if still manages to attack, reduces the affected's attack.
+
+    Regen,      //Regenerates health; +1 per level per turn
     No_Items,   //Can't use any items
     Danger,     //Low HP
-    Angry       //Increased damage, decreased defense
+    Angry       //Increased damage(+1/lev), decreased defense (-1/lev), more difficult to hit button prompts
 }
 
 public enum Elements
@@ -66,6 +115,7 @@ public enum Elements
     Fire,
     Ice,
     Wind,
+    Electricity,
     Explosion
 }
 public enum ActionCommandType
