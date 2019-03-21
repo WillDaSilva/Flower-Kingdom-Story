e83a0013fb15c0fca2f77f616b4cb283224a3e6d
NeatCrown <chol_peter@msn.com>
2019-03-20 21:20:39 -0700

    Modify StatusEffect
    Removed MonoBehaviour Inheritance
    Made variables public
    Added statusEffectReapplyBehaviour and length

--- Assets/Scripts/StatusEffects/StatusEffect.cs
+++ Assets/Scripts/StatusEffects/StatusEffect.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class StatusEffect : MonoBehaviour
+public abstract class StatusEffect
 {
-    StatusEffects effect; // identifier - matches enum
-    StatusEffectType type; // Indicates if effect is a buff, a debuff, or neutral
-    int level; // The strength of the effect; default is 0
+    public StatusEffects effect; // identifier - matches enum
+    public StatusEffectType type; // Indicates if effect is a buff, a debuff, or neutral
+    public StatusEffectReapplyBehaviour reapplyBehaviour;
+    public int level; // The strength of the effect; default is 0
+    public int length; // The length of the effect; default is 1. if WorldStatusEffect, length is number of ticks. -1 means infinite.
 }
 
 public abstract class BattleStatusEffect : StatusEffect
