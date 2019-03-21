5708534e3559b54d595b537bb1fcf2762f64cfb1
NeatCrown <chol_peter@msn.com>
2019-03-20 22:44:28 -0700

    Add Spotlight Targetter
    Points the forward vector towards a transform.

---
+++ Assets/Scripts/SpotlightTargeter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightTargeter : MonoBehaviour {
+    public Transform target;
+    new Light light;
+	// Use this for initialization
+	void Start ()
+    {
+        light = GetComponent<Light>();//InChildren<Light>();
+        //target = transform.GetChild(1);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Quaternion rot = Quaternion.LookRotation(target.position, Vector3.up);
+        light.transform.rotation = Quaternion.Lerp(light.transform.rotation, rot, 0.25f);*/
+
+        Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position + Vector3.up);
+        if (target.position.x > 13.5f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
+        }
+        else
+        {
+            targetRotation = Quaternion.Euler(2, 0, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
+        }
+    }
+}
