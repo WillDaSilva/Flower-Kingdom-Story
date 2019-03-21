952b1e6eb5c096e90050d30d8e8a8c9d1cc5c0c8
NeatCrown <chol_peter@msn.com>
2019-03-20 21:18:12 -0700

    Modify Joystick
    Added RStick and DPad.
    Renamed NormInput to NormalizedInput

--- Assets/Scripts/Joysticks.cs
+++ Assets/Scripts/Joysticks.cs
@@ -5,15 +5,20 @@ public class Joysticks : MonoBehaviour {
     
     public static bool Jump, JumpUp, JumpDown, JumpPress;
 
-    public static Joystick LStick, RStick;
+    public static Joystick LStick, RStick, DPad;
     void Start()
     {
         LStick = new Joystick();
         LStick.stepAmount = 3;
+        RStick = new Joystick();
+        RStick.stepAmount = 3;
+        DPad = new Joystick();
+        DPad.stepAmount = 1;
     }
 	void Update () {
         LStick.UpdateAxes(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
+        RStick.UpdateAxes(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+        DPad.UpdateAxes(Input.GetAxis("HorizontalD"), Input.GetAxis("VerticalD"));
         Jump = Input.GetButton("Jump");
         JumpDown = Input.GetButtonDown("Jump");
         JumpUp = Input.GetButtonUp("Jump");
@@ -24,15 +29,15 @@ public class Joysticks : MonoBehaviour {
 [System.Serializable]
 public class Joystick
 {
-    public Vector3 NormInput, CappedInput, StepInput;
+    public Vector3 NormalizedInput, CappedInput, StepInput;
     public float stepMagnitude;
     public float stepAmount;
 
     public void UpdateAxes(float h, float v)
     {
-        NormInput = new Vector3(h, 0, v).normalized;
+        NormalizedInput = new Vector3(h, 0, v).normalized;
         CappedInput = Vector3.ClampMagnitude(new Vector3(h, 0, v), 1);
         stepMagnitude = Mathf.Round(stepAmount * CappedInput.magnitude) / stepAmount;
-        StepInput = NormInput * stepMagnitude;
+        StepInput = NormalizedInput * stepMagnitude;
     }
 }
